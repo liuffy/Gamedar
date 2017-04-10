32dc3c51f5fb069882443330628ab3394f74a3ed
Sophia Liu <sliu3@wellesley.edu>
2017-04-09 21:04:42 -0700

    File setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/gamedar.jsx",
+  output: {
+    path: path.join(__dirname,'frontend'),
+    filename: "bundle.js"
+  },
+   node: {
+      net: 'empty',
+      tls: 'empty',
+      dns: 'empty',
+      fs: "empty"
+    },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  // include source-map devtool
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx" ]
+  }
+};
